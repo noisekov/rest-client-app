f77bb430ba86027140573b4f3f2070f0dec89f92
Larisa Novozhilova <kara4516@gmail.com>
2025-04-05 12:02:49 +0300

    feat: add skeleton and user name

--- rest-client-app/src/app/[locale]/page.tsx
+++ rest-client-app/src/app/[locale]/page.tsx
@@ -1,26 +1,32 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+import { useAuthStore } from '@/store/authStore';
 
 export default function MainPage() {
   const t = useTranslations('MainPage');
-  const auth = 'auth';
+  const { user, loading } = useAuthStore();
 
-  return auth === 'auth' ? (
-    <div className="flex flex-col gap-4 items-center">
-      <p>{t('welcome')}!</p>
-      <div className="flex gap-4">
-        <Button variant="outline" size="lg" asChild>
-          <Link href="/signin">{t('sign_in')}</Link>
-        </Button>
-        <Button size="lg" asChild>
-          <Link href="/signup">{t('sign_up')}</Link>
-        </Button>
+  if (loading) {
+    return (
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
       </div>
-    </div>
-  ) : (
+    );
+  }
+
+  return user ? (
     <div className="flex flex-col gap-4 items-center">
-      <div>{t('welcomeBack')}, User!</div>
+      <div>
+        {t('welcomeBack')}, {user.displayName}!
+      </div>
       <div className="flex gap-4">
         <Button size="lg" asChild>
           <Link href="/restful">REST Client</Link>
@@ -30,6 +36,18 @@ export default function MainPage() {
         </Button>
         <Button variant="outline" size="lg" asChild>
           <Link href="/variables">{t('variables')}</Link>
+        </Button>
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col gap-4 items-center">
+      <p>{t('welcome')}!</p>
+      <div className="flex gap-4">
+        <Button variant="outline" size="lg" asChild>
+          <Link href="/signin">{t('sign_in')}</Link>
+        </Button>
+        <Button size="lg" asChild>
+          <Link href="/signup">{t('sign_up')}</Link>
         </Button>
       </div>
     </div>
