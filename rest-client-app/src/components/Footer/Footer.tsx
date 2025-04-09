4ce3519d87ee033f0a42e102e073320c793c5ac7
Larisa Novozhilova <kara4516@gmail.com>
2025-04-09 15:17:22 +0300

    fix: replaced Link with a tag

--- rest-client-app/src/components/Footer/Footer.tsx
+++ rest-client-app/src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import logoRS from '../../../public/rss-logo.svg';
 import logoGitHub from '../../../public/github-logo.png';
@@ -10,7 +9,7 @@ export function Footer() {
       <div className="max-w-7xl mx-auto w-full px-4 row-start-3 grid gap-[24px] grid-cols-3 grid-flow-row border-t border-dashed border-[#cfcfcf] border-t-px pt-[20px] pb-[20px]">
         <div className="flex flex-row items-start flex-wrap">
           <Button variant="link">
-            <Link
+            <a
               className="flex items-center gap-[5px]"
               href="https://github.com/noisekov"
               target="_blank"
@@ -22,11 +21,11 @@ export function Footer() {
                 height={20}
               ></Image>
               noisekov
-            </Link>
+            </a>
           </Button>
 
           <Button variant="link">
-            <Link
+            <a
               className="flex items-center gap-[5px]"
               href="https://github.com/skayer81"
               target="_blank"
@@ -38,11 +37,11 @@ export function Footer() {
                 height={20}
               ></Image>
               skayer81
-            </Link>
+            </a>
           </Button>
 
           <Button variant="link">
-            <Link
+            <a
               className="flex items-center gap-[5px]"
               href="https://github.com/LaraNU"
               target="_blank"
@@ -54,7 +53,7 @@ export function Footer() {
                 height={20}
               ></Image>
               LaraNU
-            </Link>
+            </a>
           </Button>
         </div>
 
@@ -63,14 +62,14 @@ export function Footer() {
         </div>
 
         <div className="flex items-center gap-4 justify-self-end">
-          <Link
+          <a
             href="https://rs.school/courses/reactjs"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-1 text-sm font-medium hover:text-primary transition-colors"
           >
             <Image src={logoRS} alt="RS" width={40} height={40}></Image>
-          </Link>
+          </a>
         </div>
       </div>
     </footer>
