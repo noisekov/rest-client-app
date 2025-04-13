eed9b21449ef1078157fd744cbdcde491e857122
Larisa Novozhilova <kara4516@gmail.com>
2025-04-13 19:53:57 +0300

    fix: add check missing variables

--- rest-client-app/src/utils/replaceVariables.ts
+++ rest-client-app/src/utils/replaceVariables.ts
@@ -1,6 +1,14 @@
 export function replaceVariables(
   str: string,
-  variables: Record<string, string>
-) {
-  return str.replace(/\{\{(\w+)\}\}/g, (_, key) => variables[key] ?? '');
+  variables: Record<string, string>,
+  onMissing?: (key: string) => void
+): string {
+  return str.replace(/\{\{(\w+)\}\}/g, (_, key) => {
+    if (key in variables) {
+      return variables[key];
+    } else {
+      onMissing?.(key);
+      return `{{${key}}}`;
+    }
+  });
 }
