8245afe386f41cea736da2f853f790b4c0f12b29
Larisa Novozhilova <kara4516@gmail.com>
2025-04-19 18:49:16 +0300

    fix: config

--- rest-client-app/playwright.config.ts
+++ rest-client-app/playwright.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './',
+  testMatch: '**/*.spec.{ts,tsx}',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -15,15 +16,11 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000/en',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
