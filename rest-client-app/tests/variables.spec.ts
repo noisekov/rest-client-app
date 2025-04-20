15d00b6a7c74b7af38e3eafa4d696065696f13ca
noisekov <noise123q@mail.ru>
2025-04-20 18:38:16 +0300

    fix: remove console.log

--- rest-client-app/tests/variables.spec.ts
+++ rest-client-app/tests/variables.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test.describe('Variables page', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/en/variables');
-    console.log(page.url());
   });
 
   test('has title', async ({ page }) => {
